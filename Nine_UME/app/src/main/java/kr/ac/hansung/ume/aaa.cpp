97a32c973c7cfebd4f758ec6fe3a440481ed1a80
hellonayeon <nayeonkwon9815@gmail.com>
2019-11-29 14:33:58 +0900

    Test Commit

---
+++ Nine_UME/app/src/main/java/kr/ac/hansung/ume/aaa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 권나연 on 2019-11-29.
+//
+
+#include "aaa.h"
---
+++ Nine_UME/app/src/main/java/kr/ac/hansung/ume/aaa.h
@@ -0,0 +1,16 @@
+//
+// Created by 권나연 on 2019-11-29.
+//
+
+#ifndef TERMPROJECT_AAA_H
+#define TERMPROJECT_AAA_H
+
+
+
+class aaa {
+
+};
+
+
+
+#endif //TERMPROJECT_AAA_H
